777648fcc32abf44446b993cfe1f13ed5847984f
reiffle <pederreiff@gmail.com>
2025-05-25 23:18:22 -0500

    Created New Server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module workspace/github.com/reiffle/chirpy/v2
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+)
+
+func main() {
+mux := http.NewServeMux()
+
+s:=&http.Server{
+Addr: ":8080",
+Handler: mux,
+}
+
+err := s.ListenAndServe()
+if err!=nil{
+    fmt.Println("Couldn't start server")
+    os.Exit(1)
+    }
+}
+
